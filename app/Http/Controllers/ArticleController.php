2f6c11a2930273c631a22f3ed234c27a41cc3553
PhonePyaeKo <phonepyaeko01@gmail.com>
2025-09-18 21:22:56 +0630

    Create Database & Sample data seeding

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $data = [
-            ['title' => 'First Article'],
-            ['title' => 'Second Article']
-        ];
+        $data = Article::all();
 
         return  view("articles.index", [
             'articles' => $data,
