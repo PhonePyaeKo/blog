a4202a2cd8dcdb541f583cdc0970bf11d2734a81
PhonePyaeKo <phonepyaeko01@gmail.com>
2025-09-21 15:34:31 +0630

    Add Comment and Category Model,seed sample data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,5 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\Article::factory(20)->create();
+        \App\Models\Comment::factory(40)->create();
+
+        $list = ['News', 'Tech', 'Web', 'App', 'Oss'];
+        foreach($list as $name) {
+            \App\Models\Category::create(['name' => $name]);
+        }
     }
 }
